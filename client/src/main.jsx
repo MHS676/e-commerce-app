b6fdcb883d0997f341ac0f0af18419bc09342d5c
Hasan Talukder <mahadihasansiam2@gmail.com>
2024-09-05 14:50:43 +0600

    main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { AuthProvider } from './contexts/AuthContext';
+import { ProductProvider } from './contexts/ProductContext';
+import { CartProvider } from './contexts/CartContext';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.render(
+  <React.StrictMode>
+    <AuthProvider>
+      <ProductProvider>
+        <CartProvider>
+          <App />
+        </CartProvider>
+      </ProductProvider>
+    </AuthProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
